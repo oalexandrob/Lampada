072cc071f96d247061a2ea670bd496ce68f64bea
oalexandrob <89278058+oalexandrob@users.noreply.github.com>
2021-09-28 14:39:36 -0300

    Alterações nos botões

--- Lampada/script.js
+++ Lampada/script.js
@@ -55,18 +55,47 @@ function somar(x, y){
 //função trocar imagem
 function interruptor(filename, status){
 
+                
+          
+          document.querySelector('.imagem').setAttribute('src','imagens/'+filename);
+          document.querySelector('.imagem').setAttribute('data-status', status);
+
           
      
-          document.querySelector('#imagem').setAttribute('src','imagens/'+filename);
-          document.querySelector('#imagem').setAttribute('data-status', status);
-       
-          
 }
+
+function ligada(){ 
+     
+     document.querySelector('#concertada').disabled = true;
+     
+
+}
+
+function quebrar(){ 
+     
+     document.querySelector('#ligado').disabled = true;
+     document.querySelector('#desligado').disabled = true;
+     document.querySelector('#concertada').disabled = false;
+     
+
+}
+
+
+
+function concertar(){
+     document.querySelector('#ligado').disabled = false;
+     document.querySelector('#desligado').disabled = false;
+     document.querySelector('#concertada').disabled = false;
+
+}
+
+
+
 
 
 //função mostrar um alerta informando o status da lâmpada
 function statusLampada(){
-     let status = document.querySelector('#imagem').getAttribute('data-status');
+     let status = document.querySelector('.imagem').getAttribute('data-status');
      alert("A lâmpada está "+status);
 }
 
