cb51c39c59da285e1cf8a9750ae35e066c72933d
oalexandrob <89278058+oalexandrob@users.noreply.github.com>
2021-09-28 14:48:56 -0300

    alteração no javascript

--- Lampada/script.js
+++ Lampada/script.js
@@ -1,88 +1,37 @@
-//função para a div ficar azul
-function azul(){ 
-     document.querySelector('.conteiner').classList.remove('verde');
-     document.querySelector('.conteiner').classList.remove('vermelho');
-     document.querySelector('.conteiner').classList.add('azul');
-
-}
-
-//função para a div ficar vermelho
-function vermelho(){
-     document.querySelector('.conteiner').classList.remove('verde');
-     document.querySelector('.conteiner').classList.remove('azul');
-     document.querySelector('.conteiner').classList.add('vermelho');
-
-
-}
-
-//função para a div ficar verde
-function verde(){
-     document.querySelector('.conteiner').classList.remove('azul');
-     document.querySelector('.conteiner').classList.remove('vermelho');
-     document.querySelector('.conteiner').classList.add('verde');
-
-}
-
-//função para o botão alternar de cores
-function trocar(){
-     if (document.querySelector('#botao').classList.contains('preto')){
-          document.querySelector('#botao').classList.remove('preto');
-          document.querySelector('#botao').classList.add('verde');
-          document.querySelector('#botao').innerText = 'Verde';
-          
-          
-
-     } else {
-
-     document.querySelector('#botao').classList.remove('verde');
-     document.querySelector('#botao').classList.add('preto');
-     document.querySelector('#botao').innerText = 'Preto';
-     
-     
-
-     }
-}
-
-//função somar
-function somar(x, y){
-     let total = x + y;
-          
-     return total;
-     
-}
-
-
-//função trocar imagem
+//função trocar as imagens
 function interruptor(filename, status){
 
                 
           
-          document.querySelector('.imagem').setAttribute('src','imagens/'+filename);
-          document.querySelector('.imagem').setAttribute('data-status', status);
+     document.querySelector('.imagem').setAttribute('src','imagens/'+filename);
+     document.querySelector('.imagem').setAttribute('data-status', status);
 
-          
      
+
 }
 
+//Função para a lâmpada ligar
 function ligada(){ 
-     
+
      document.querySelector('#concertada').disabled = true;
-     
+
 
 }
 
+//Função para quebrar a lampada
 function quebrar(){ 
-     
+
      document.querySelector('#ligado').disabled = true;
      document.querySelector('#desligado').disabled = true;
      document.querySelector('#concertada').disabled = false;
-     
+
 
 }
 
 
-
+//função para concertar a lâmpada
 function concertar(){
+
      document.querySelector('#ligado').disabled = false;
      document.querySelector('#desligado').disabled = false;
      document.querySelector('#concertada').disabled = false;
@@ -95,7 +44,7 @@ function concertar(){
 
 //função mostrar um alerta informando o status da lâmpada
 function statusLampada(){
-     let status = document.querySelector('.imagem').getAttribute('data-status');
-     alert("A lâmpada está "+status);
+let status = document.querySelector('.imagem').getAttribute('data-status');
+alert("A lâmpada está "+status);
 }
 
